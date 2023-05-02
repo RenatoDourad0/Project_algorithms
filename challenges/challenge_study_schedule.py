bb70c08b89c28fd0d3a8617ea04376b5900997eb
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-05-02 17:38:41 -0300

    fix: req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -6,6 +6,6 @@ def study_schedule(permanence_period, target_time):
     for period in permanence_period:
         if not isinstance(period[0], int) or not isinstance(period[1], int):
             return None
-        if range(period[0], period[1] + 1).count(target_time) == 1:
+        if period[0] < target_time < period[1]:
             active_users += 1
     return active_users
