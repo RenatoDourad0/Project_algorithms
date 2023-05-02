e5ecbf7c149dba0ca9fc7079b71944848be0806b
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-05-02 17:00:51 -0300

    feta: req 1 - O(n2)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,7 +4,7 @@ def study_schedule(permanence_period, target_time):
 
     active_users = 0
     for period in permanence_period:
-        if not isinstance(period, tuple) or period[0] > period[1]:
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
             return None
         if target_time in list(range(period[0], period[1] + 1)):
             active_users += 1
